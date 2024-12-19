0cf7e4ad6903d6e0da5ee817a21cb9685428848e
PeAcePh1NiX <savage89100@gmail.com>
2024-12-19 22:50:46 +0545

    Upadated JWT stat

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -17,7 +17,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     console.log('Inside JWT Strategy Validate Method');
     console.log('Payload:', payload);
     console.log('Authenticated User ID:', payload.payload.hotelId);
-
     return { HotelId: payload.payload.hotelId };
   }
 }
